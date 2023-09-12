6e1a123cad2b03438ca284a1017630906ea7cf16
Hamza Oumeziane <116696843+Omz123@users.noreply.github.com>
2023-09-12 10:37:39 -0400

    Calculer le temps
    En format hh:mm:ss

--- ThreadLivraison/Program.cs
+++ ThreadLivraison/Program.cs
@@ -1,3 +1,16 @@
 ﻿using static ThreadLivraison.ConstantesSimulation;
 
-Console.WriteLine(NOMBRE_COMMANDES);
+
+
+
+string calculerTemps(int nbMillisecondes)
+{
+    const int MILLIS_PAR_HEURE = 3600000;
+    const int MILLIS_PAR_MINUTE = 6000;
+    const int MILLIS_PAR_SECONDE = 1000;
+
+    int ss = (nbMillisecondes/ MILLIS_PAR_SECONDE) % 60;
+    int mm = (nbMillisecondes / MILLIS_PAR_MINUTE) % 60;
+    int hh = (nbMillisecondes / MILLIS_PAR_HEURE ) % 24;
+    return $"{hh}:{mm}:{ss}";  
+}
